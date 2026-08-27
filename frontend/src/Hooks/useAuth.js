5d0bd7a884923aa251f633b1e660d4109cf8efb4
John Le <90670710+Primalpancake92@users.noreply.github.com>
2026-08-27 22:53:48 +1000

    login function working

--- frontend/src/Hooks/useAuth.js
+++ frontend/src/Hooks/useAuth.js
@@ -5,49 +5,49 @@ export default function useAuth() {
     const [ error, setError ] = useState(false);
     const [ user, setUser ] = useState({});
 
-    useEffect(() => {
-        const fetchUser = async (email, password) => {
-            try {
-                setLoading(true);
+    const loginUser = async (email, password) => {
+        try {
+            setLoading(true);
 
-                const response = await fetch("http://127.0.0.1:3000/userRoutes/login",
-                    {
-                        method: "POST",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({
-                            "email": email,
-                            "password": password
-                        }),
-                        credentials: "include"
-                    }
-                );
-
-                const data = await response.json();
-
-                if (!response.ok) {
-                    throw new Error("User data unable to load.");
+            const response = await fetch("http://127.0.0.1:3000/user/login",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        "email": email,
+                        "password": password
+                    }),
+                    credentials: "include"
                 }
+            );
 
-                if (response.status === "Error") {
-                    throw new Error("Invalid user details entered.");
-                }
+            const data = await response.json();
 
-                if (!data) {
-                    throw new Error(
-                        `There are there no users with email ${email}`
-                    )
-                }
-
-                setUser(data);
-            } catch (error) {
-                console.log(error.message);
-                setError(true);
+            if (!response.ok) {
+                throw new Error("User data unable to load.");
             }
+
+            if (response.status === "Error") {
+                throw new Error("Invalid user details entered.");
+            }
+
+            if (!data) {
+                throw new Error(
+                    `There are there no users with email ${email}`
+                )
+            }
+
+            setUser(data);
+
+            return true;
+        } catch (error) {
+            console.log(error.message);
+            setError(true);
+            return false;
+        } finally {
             setLoading(false);
-        };
+        }
+    };
 
-        fetchUser();
-    }, []);
-
-    return { error, loading, user };
+    return { loginUser, error, loading, user };
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,22 +1,42 @@
-import { Link } from "react-router-dom";
-import useAuth from "../components/Login";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import useAuth from "../Hooks/useAuth";
 import Dots from "./Dots";
 
 function LoginForm() {
-    const { error, loading, user } = useAuth(email, password);
+    const navigate = useNavigate();
+    const { loginUser, error, loading, user } = useAuth();
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const login = await loginUser(email, password);
+
+        if (login) {
+            navigate("/home");
+        }
+    };
+
     return (
         <div className="relative flex w-full h-screen justify-center items-center
         z-11">
             <form className="flex flex-col p-2.5 text-white border
-            border-white w-150 h-120 rounded-3xl justify-center items-center">
+            border-white w-150 h-120 rounded-3xl justify-center items-center"
+            onSubmit={handleSubmit}>
                 <div className="flex flex-col items-center gap-5">
                     <label className="text-lg">Email</label>
                     <input placeholder="john.smith@email.com" type="email"
-                    className="px-3 py-1 rounded-full focus:border-blue-500">
+                    className="px-3 py-1 rounded-full focus:border-blue-500"
+                    value={email} onChange={(e) => setEmail(e.target.value)}>
                     </input>
                     <label className="text-lg">Password</label>
                     <input type="password" placeholder="Enter here" className="
-                    rounded-full border border-white px-3 py-1">
+                    rounded-full border border-white px-3 py-1" 
+                    value={password} onChange={(e) => setPassword(
+                        e.target.value
+                    )}>
                     </input>
                 </div>
                 <div className="relative flex justify-center items-center gap-5
@@ -27,7 +47,9 @@ function LoginForm() {
                     <button type="submit" className="relative flex justify-center
                     items-center border border-(--tertiary-colour) py-1 px-5 
                     rounded-full hover:bg-(--tertiary-colour) transition-colors
-                    duration-300 ease-in-out hover:text-(--primary-colour)">Login</button>
+                    duration-300 ease-in-out hover:text-(--primary-colour)">
+                        Login
+                    </button>
                 </div>
             </form>
         </div>
